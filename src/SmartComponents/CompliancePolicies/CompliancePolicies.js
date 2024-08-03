2bedd075ac3938d089b743883065775e31668c1e
AsToNlele <theastonofficial@gmail.com>
2024-08-03 17:21:44 +0200

    xd

--- src/SmartComponents/CompliancePolicies/CompliancePolicies.js
+++ src/SmartComponents/CompliancePolicies/CompliancePolicies.js
@@ -15,11 +15,38 @@ import {
   LinkButton,
 } from 'PresentationalComponents';
 import { usePoliciesQuery } from '../../Utilities/hooks/usePoliciesQuery/usePoliciesQuery';
+import { useQuery, gql } from '@apollo/client';
+import useAPIV2FeatureFlag from '../../Utilities/hooks/useAPIV2FeatureFlag';
 
-export const CompliancePolicies = () => {
+const QUERY = gql`
+  {
+    profiles(search: "external = false and canonical = false") {
+      edges {
+        node {
+          id
+          name
+          description
+          refId
+          complianceThreshold
+          totalHostCount
+          osMajorVersion
+          policyType
+          policy {
+            id
+            name
+          }
+          businessObjective {
+            id
+            title
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const CompliancePoliciesBase = ({ query }) => {
   const location = useLocation();
-  let { data, loading, error, refetch } = usePoliciesQuery();
-
   const CreateLink = () => (
     <Link
       to="/scappolicies/new"
@@ -33,16 +60,23 @@ export const CompliancePolicies = () => {
     </Link>
   );
 
+  let { data, error, loading, refetch } = query;
   useEffect(() => {
     refetch();
   }, [location, refetch]);
   let policies;
 
-  if (Array.isArray(data?.data)) {
+  if (data) {
     error = undefined;
-    loading = false;
-    policies = data.data;
+    loading = undefined;
+    policies = data.profiles.edges.map((profile) => profile.node);
   }
+
+  console.log('data', data);
+
+  console.log('query', query);
+
+  console.log('base policies', policies);
 
   return (
     <React.Fragment>
@@ -75,4 +109,92 @@ export const CompliancePolicies = () => {
   );
 };
 
-export default CompliancePolicies;
+const CompliancePoliciesV2 = () => {
+  const query = usePoliciesQuery();
+  console.log(query);
+  let newData = [];
+  if (query.data?.data) {
+    newData = query.data.data.map((policy) => serialize(policy));
+  }
+  console.log(newData);
+  const transformedData = {
+    profiles: {
+      edges: newData.map((policy) => {
+        return { node: policy };
+      }),
+    },
+  };
+  console.log(transformedData);
+
+  return <CompliancePoliciesBase query={{ ...query, data: transformedData }} />;
+};
+
+const CompliancePoliciesGraphQL = () => {
+  let query = useQuery(QUERY);
+  return <CompliancePoliciesBase query={query} />;
+};
+
+const CompliancePoliciesWrapper = () => {
+  // const apiV2Enabled = useAPIV2FeatureFlag();
+  const apiV2Enabled = false;
+  // const apiV2Enabled = true;
+
+  return apiV2Enabled ? (
+    <CompliancePoliciesV2 />
+  ) : (
+    <CompliancePoliciesGraphQL />
+  );
+};
+
+const serialize = (input) => {
+  const settings = {
+    id: 'id',
+    title: 'title',
+    description: 'description',
+    business_objective: 'businessObjective.title',
+    compliance_threshold: 'complianceThreshold',
+    total_system_count: 'totalHostCount',
+    os_major_version: 'osMajorVersion',
+    profile_title: 'policy.name',
+    ref_id: 'refId',
+  };
+  const getValue = (obj, key) => {
+    let ref = obj;
+    if (key.includes('.')) {
+      key.split('.').forEach((element) => {
+        ref = ref[element];
+      });
+    } else {
+      ref = ref[key];
+    }
+    return ref;
+  };
+
+  let newObj = {};
+  for (const [key, value] of Object.entries(settings)) {
+    const finalValue = getValue(input, key);
+    if (Array.isArray(value)) {
+      console.log('xd');
+    } else {
+      if (value.includes('.')) {
+        const split = value.split('.');
+        let ref = newObj;
+        split.forEach((el, index) => {
+          if (!Object.prototype.hasOwnProperty.call(ref, el)) {
+            ref[el] = index + 1 !== split.length ? {} : finalValue;
+          }
+          ref = ref[el];
+        });
+      } else {
+        if (key === 'os_major_version') {
+          newObj[value] = input[key].toString();
+        } else {
+          newObj[value] = input[key];
+        }
+      }
+    }
+  }
+  return newObj;
+};
+
+export default CompliancePoliciesWrapper;
